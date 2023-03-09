b6ffb0b5971299cafc1d6685eb10094cf19f29b9
MajorEnkidu <kyle.klus.2@gmail.com>
2023-03-09 18:45:02 +0100

    - Quick favicon fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,8 +23,25 @@ export default function Home() {
           content="width=device-width, initial-scale=1"
         />
         <link
-          rel="icon"
+          rel="shortcut icon"
           href="/favicon.ico"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
         />
       </Head>
       <Header />
