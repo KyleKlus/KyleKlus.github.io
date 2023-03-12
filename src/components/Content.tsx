7761405d451ce0678416dacc8dd9bf18a606af39
MajorEnkidu <kyle.klus.2@gmail.com>
2023-03-12 22:19:09 +0100

    - Improved scroll behavior - Fixed some bugs

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,5 +4,5 @@ import styles from '@/styles/Content.module.css';
 interface IContentProps { }
 
 export default function Content(props: React.PropsWithChildren<IContentProps>) {
-  return <div className={styles.content}>{props.children}</div>;
+  return <div id='content' className={styles.content}>{props.children}</div>;
 }
