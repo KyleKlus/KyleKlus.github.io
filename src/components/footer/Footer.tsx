9f4297066a52c0de86ad74abeaa4cb2c127cc0b4
MajorEnkidu <kyle.klus.2@gmail.com>
2023-03-25 11:54:09 +0100

    -

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,10 @@
 
 import styles from '@/styles/components/footer/Footer.module.css';
 import Link from 'next/link';
-import NavLink from '../header/NavLink';
-import ScrollNavLink from '@/components/header/ScrollNavLink';
 
-export default function Footer() {
+interface IFooterProps { }
+
+export default function Footer(props: React.PropsWithChildren<IFooterProps>) {
   return (
     <>
       <footer className={styles.footer}>
@@ -14,29 +14,7 @@ export default function Footer() {
           <Link href={'https://github.com/MajorEnkidu'}>Kyle Klus</Link>
         </p>
         <div className={styles.navLinkWrapper}>
-          <ScrollNavLink
-            className={styles.footerNavLink}
-            elementName="/#heroPage"
-            displayText="Home"
-          />
-          <ScrollNavLink
-            className={styles.footerNavLink}
-            elementName="/#portfolioPage"
-            displayText="Portfolio"
-          />
-          <ScrollNavLink
-            className={styles.footerNavLink}
-            elementName="/#aboutPage"
-            displayText="About"
-          />
-          <Link href={'https://github.com/MajorEnkidu'} className={styles.footerNavLink}>GitHub</Link>
-          <Link href={'https://ko-fi.com/majorenkidu'} className={styles.footerNavLink}>Ko-fi</Link>
-          <Link href={'mailto:kyle.klus.2@gmail.com'} className={styles.footerNavLink}>Contact</Link>
-          <NavLink
-            className={styles.sideNavLink + ' ' + styles.footerNavLink}
-            pathName="/privacy"
-            displayText="Privacy"
-          />
+          {props.children}
         </div>
       </footer>
     </>
