d11ab823dbdaafe1e8817ecc2d4cf9776276a29e
MajorEnkidu <kyle.klus.2@gmail.com>
2023-10-28 22:59:15 +0200

    - Fix

--- src/components/links/NavLink.tsx
+++ src/components/links/NavLink.tsx
@@ -23,8 +23,8 @@ export default function NavLink(props: React.PropsWithChildren<INavLinkProps>) {
   const lastPropsPathNameElement: string = props.pathName.split('/').reverse()[0].replace('#', '');
 
   const styleClass =
-    (lastPropsPathNameElement.length!== 0 && lastBasePathElement.length !== 0 && lastPropsPathNameElement.indexOf(lastBasePathElement) !== -1) ||
-      (lastPropsPathNameElement.length!== 0 && lastPathNameElement.length !== 0 && lastPropsPathNameElement.indexOf(lastPathNameElement)) !== -1
+    (lastPropsPathNameElement.length === 0 && lastPathNameElement.length === 0) ||
+      (lastPropsPathNameElement.length !== 0 && lastPathNameElement.length !== 0 && lastPropsPathNameElement.indexOf(lastPathNameElement) !== -1)
       ? styles.isCurrentWindow
       : '';
 
