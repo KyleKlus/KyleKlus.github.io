685d2e9eca44670b1571a3c650585c2bac8632aa
MajorEnkidu <kyle.klus.2@gmail.com>
2023-10-28 22:50:11 +0200

    .

--- src/components/links/NavLink.tsx
+++ src/components/links/NavLink.tsx
@@ -22,8 +22,6 @@ export default function NavLink(props: React.PropsWithChildren<INavLinkProps>) {
   const lastPathNameElement: string = router.pathname.split('/').reverse()[0].replace('#', '');
   const lastPropsPathNameElement: string = props.pathName.split('/').reverse()[0].replace('#', '');
 
-  console.log(router, lastBasePathElement, lastPathNameElement, lastPropsPathNameElement);
-
   const styleClass =
     (lastPropsPathNameElement.length!== 0 && lastBasePathElement.length !== 0 && lastPropsPathNameElement.indexOf(lastBasePathElement) !== -1) ||
       (lastPropsPathNameElement.length!== 0 && lastPathNameElement.length !== 0 && lastPropsPathNameElement.indexOf(lastPathNameElement)) !== -1
