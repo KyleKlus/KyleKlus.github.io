15ae90f9fe04756fb944e1b203e23487bd91e085
KyleKlus <kyle.klus.2@gmail.com>
2024-01-21 18:02:38 +0100

    - Removed projects from main website

--- src/interfaces/IProjectCategory.ts
+++
@@ -1,8 +0,0 @@
-import IProjectPost from "./IProjectPost";
-
-export default interface IProjectCategory {
-    title: string;
-    url: string;
-    otherCategories: IProjectCategory[];
-    posts: IProjectPost[];
-}
